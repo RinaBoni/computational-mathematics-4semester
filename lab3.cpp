09024fc8e3d547efcfd488ad953b3a94499c9386
RinaBoni <83748388+RinaBoni@users.noreply.github.com>
2022-05-12 13:37:20 +0900

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -1,17 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
+#include <vector>
 
 using namespace std;
+
+float xi[6] = { -1.4, -0.4, 0.2, 1.3, 2, 3.3 };
+float yi[6] = { 6.50496, -4.43904, 4.13952, 28.51563, 24, 6.76863 };
+
+float x = -0.5;
 
 //Интерполяция по Лагранжу
 float lagrange(float x, float* xi, float* yi);
 //Интерполяция по Ньютону
 float newtone(float x, float* xi, float* yi);
+//разделённые разности
+//float razdelen(float x, float* xi, float* yi);
+vector<vector<float>> razdelen();
 
 int main()
 {
-    float x = -0.5;
-    float xi[6] = { -1.4, -0.4, 0.2, 1.3, 2, 3.3 };
-    float yi[6] = { 6.50496, -4.43904, 4.13952, 28.51563, 24, 6.76863 };
+    
+    
 
     cout << "Newtone: " << newtone(x, xi, yi) << endl;
     cout << "Lagranje: " << lagrange(x, xi, yi) << endl;
@@ -43,11 +51,61 @@ float lagrange(float x, float* xi, float* yi)
                 zman = zman * (xi[k] - xi[j]);
         }
 
-        lag = lag + yi[k] * chis * zman;
+        lag = lag + yi[k] * chis  /zman;
     }
     return lag;
 }
 
+//разделённые разности
+
+//float razdelen(float x, float* xi, float* yi)
+//{
+//    int n = 6;
+//
+//
+//
+//    /*for (int m = 1; m < n; m++)
+//    {
+//        for (int k = 0; k < (n - m); k++)
+//        {
+//            if(m==1)
+//
+//        }
+//    }*/
+//}
+
+vector<vector<float>> razdelen()
+{
+    int n = 6;
+
+    vector<vector<float>> a(n, vector<float>(n + 1));
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        a[i][0] = xi[i];
+        a[i][1] = yi[i];
+    }
+
+    int k = 0;
+
+    for (int j = 2; j < a[0].size(); j++)
+    {
+        for (int i = 0; i < a.size() - 1; i++)
+        {
+            if (i + 1 + k >= a.size())
+            {
+                break;
+            }
+            else
+            {
+                a[i][j] = (a[i + 1][j - 1] - a[i][j - 1]) / (a[i + 1 + k][0] - a[i][0]);
+            }
+
+        }
+        k++;
+    }
+    return a;
+}
 
 //Интерполяция по Ньютону
 float newtone(float x, float* xi, float* yi)
